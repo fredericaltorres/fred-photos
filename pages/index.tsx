12631f0d9dac67e8ecb919447881f67f94f8922c
frederic.torres@bigtincan.com <fredericaltorres@gmail.com>
2025-12-09 21:21:22 -0500

    Update...

---
+++ pages/index.tsx
@@ -0,0 +1,161 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useRef } from "react";
+import Bridge from "../components/Icons/Bridge";
+import Logo from "../components/Icons/Logo";
+import Modal from "../components/Modal";
+import cloudinary from "../utils/cloudinary";
+import getBase64ImageUrl from "../utils/generateBlurPlaceholder";
+import type { ImageProps } from "../utils/types";
+import { useLastViewedPhoto } from "../utils/useLastViewedPhoto";
+
+/*
+
+https://vercel.com/blog/building-a-fast-animated-image-gallery-with-next-js
+https://cloudinary.com/documentation/transformation_reference
+https://www.flickr.com/photos/138302041@N06/
+
+nusbio2
+https://console.cloudinary.com/app/c-f365f09c777553e8ebf1ea4f54d6f7/assets/media_library/folders/cd8301c579893e3a49bd9528788fc09474?view_mode=mosaic
+
+*/
+
+const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
+
+  console.log(`images ${images.length}`);
+
+  const router = useRouter();
+  const { photoId } = router.query;
+  const [lastViewedPhoto, setLastViewedPhoto] = useLastViewedPhoto();
+  const lastViewedPhotoRef = useRef<HTMLAnchorElement>(null);
+
+  useEffect(() => {
+    // This effect keeps track of the last viewed photo in the modal to keep the index page in sync when the user navigates back
+    if (lastViewedPhoto && !photoId) {
+      lastViewedPhotoRef.current.scrollIntoView({ block: "center" });
+      setLastViewedPhoto(null);
+    }
+  }, [photoId, lastViewedPhoto, setLastViewedPhoto]);
+
+  return (
+    <>
+      <Head>
+        <title>Next.js Conf 2022 Photos</title>
+        <meta
+          property="og:image"
+          content="https://nextjsconf-pics.vercel.app/og-image.png"
+        />
+        <meta
+          name="twitter:image"
+          content="https://nextjsconf-pics.vercel.app/og-image.png"
+        />
+      </Head>
+      <main className="mx-auto max-w-[1960px] p-4">
+        {photoId && (
+          <Modal
+            images={images}
+            onClose={() => {
+              setLastViewedPhoto(photoId);
+            }}
+          />
+        )}
+        <div className="columns-1 gap-4 sm:columns-2 xl:columns-3 2xl:columns-4">
+          <div className="after:content relative mb-1 flex flex-col  gap-4 overflow-hidden rounded-lg bg-white/10 px-6 pb-16 pt-64 text-white shadow-highlight after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight lg:pt-0">
+            <h1 className="mt-2 mb-1 text-base font-bold tracking-widest">
+              Nusbio /2 - 2025
+            </h1>
+            <div className="">
+              Nusbio /2 + FT232H.NET Library. <br /><br />
+
+              The .NET/Windows library FT232H.NET provides an abstraction to program
+
+              <ul>
+                <li>- The SPI protocol</li>
+                <li>- The I2C protocol</li>
+                <li>- The GPIOs</li>
+              </ul>
+              for the FTDI chip FT232H using the (
+              <a href="https://www.adafruit.com/product/2264">Adafruit Breakout FT232H</a>
+              ) or any other compatible breakout.
+
+            </div>
+          </div>
+          {images.map(({ id, public_id, format, blurDataUrl }) => (
+            <Link
+              key={id}
+              href={`/?photoId=${id}`}
+              as={`/p/${id}`}
+              ref={id === Number(lastViewedPhoto) ? lastViewedPhotoRef : null}
+              shallow
+              className="after:content group relative mb-5 block w-full cursor-zoom-in after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight"
+            >
+              <Image
+                alt="Next.js Conf photo"
+                className="transform rounded-lg brightness-90 transition will-change-auto group-hover:brightness-110"
+                style={{ transform: "translate3d(0, 0, 0)" }}
+                placeholder="blur"
+                blurDataURL={blurDataUrl}
+                src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${public_id}.${format}`}
+                width={720}
+                height={480}
+                sizes="(max-width: 640px) 100vw,
+                  (max-width: 1280px) 50vw,
+                  (max-width: 1536px) 33vw,
+                  25vw"
+              />
+            </Link>
+          ))}
+        </div>
+      </main>
+      <footer className="p-6 text-center text-white/80 sm:p-12">
+        Thank you to Fred
+      </footer>
+    </>
+  );
+};
+
+export default Home;
+
+export async function getStaticProps() {
+
+  console.log(`*** getStaticProps **********************************`);
+  console.log(`process.env.CLOUDINARY_FOLDER ${process.env.CLOUDINARY_FOLDER}`);
+  console.log(`*************************************`);
+
+  const results = await cloudinary.v2.search
+    .expression(`folder:${process.env.CLOUDINARY_FOLDER}/*`)
+    .sort_by("public_id", "desc")
+    .max_results(400)
+    .execute();
+  let reducedResults: ImageProps[] = [];
+
+  let i = 0;
+  for (let result of results.resources) {
+    reducedResults.push({
+      id: i,
+      height: result.height,
+      width: result.width,
+      public_id: result.public_id,
+      format: result.format,
+    });
+    i++;
+  }
+
+  const blurImagePromises = results.resources.map((image: ImageProps) => {
+    return getBase64ImageUrl(image);
+  });
+  const imagesWithBlurDataUrls = await Promise.all(blurImagePromises);
+
+  for (let i = 0; i < reducedResults.length; i++) {
+    reducedResults[i].blurDataUrl = imagesWithBlurDataUrls[i];
+  }
+
+  return {
+    props: {
+      images: reducedResults,
+    },
+  };
+}
